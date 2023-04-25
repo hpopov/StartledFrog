f9362ed1d82f055172b10879622ad71c8be6412f
Gregory Popov <tordektopor@gmail.com>
2023-04-25 18:55:49 +0100

    Make the project runnable🥳
    (add FX modules according to https://openjfx.io/openjfx-docs/#install-javafx)

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -Dfile.encoding=UTF-8 -cp StartledFrog-jar-with-dependencies.jar;lib/* net.atlassian.cmathtutor.VplApplication
+java -Dfile.encoding=UTF-8 -cp StartledFrog-jar-with-dependencies.jar;lib/* --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml net.atlassian.cmathtutor.VplApplication
